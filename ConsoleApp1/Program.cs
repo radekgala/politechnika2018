cad32b7b6baa47e7a4f2a14fbb3e9d418ef5e2f7
michal.tomala <michal.tomala@krollontrack.com>
2018-10-11 11:06:13 +0200

    added branch1 method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,12 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Branch1();
+        }
+
+        static void NewMethod1()
+        {
+            Console.WriteLine("Branch1");
         }
     }
 }
